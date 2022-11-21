c6341ec1339cb12253697f2d11d56ddd68d89676
Damon German <damon.german@hyland.com>
2022-11-21 10:52:46 -0600

    made some changes

---
+++ functional-thinking/functional-thinking/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace functional_thinking;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
