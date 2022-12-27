ec292b176869c825fbb8627b665162e4a795c3d3
Damon German <damon.german@hyland.com>
2022-12-27 17:07:16 -0600

    made some changes

--- functional-thinking/functional-thinking/FirstClassObjects.cs
+++ functional-thinking/functional-thinking/FirstClassObjects.cs
@@ -1,6 +1,6 @@
 ﻿namespace functional_thinking;
 
-public static class FirstClassObjects
+public class FirstClassObjects
 {
-   static Func<double, double, double> Multiply = (x, y) => x * y;
+    
 }
--- functional-thinking/functional-thinking/FirstClassObjectsTests.cs
+++ functional-thinking/functional-thinking/FirstClassObjectsTests.cs
@@ -6,11 +6,31 @@ public class FirstClassObjectsTests
     [Test]
     public void MultiplyTest()
     {
+        // declare a function that multiplies two numbers
         Func<double, double, double> multiply = (x, y) => x * y;
-        
-        var myFunc = multiply;
-        
-        
+
         Assert.That(multiply(2, 3), Is.EqualTo(6));
+
+        // assign the function to a variable
+        var multiply2 = multiply;
+
+        // inset the functions into an array
+        var multiplications = new[] { multiply, multiply2 };
+
+        // call the function from the array
+        foreach (var multiplication in multiplications)
+        {
+            Assert.That(multiplication(2, 3), Is.EqualTo(6));
+        }
+
+        // Pass the function to another function
+        var result = ApplyFunction(multiply, 2, 3);
+
+        double ApplyFunction(Func<double, double, double> func, double x, double y)
+        {
+            return func(x, y);
+        }
+
+        Assert.That(result, Is.EqualTo(6));
     }
 }
